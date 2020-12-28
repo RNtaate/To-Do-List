3baa07ad1890264977ab94923ad6428cf3fb6522
RNtaate <royhyde14@gmail.com>
2020-12-28 18:42:23 +0300

    Add close-form-btn span in category form

--- src/forms.js
+++ src/forms.js
@@ -1,5 +1,5 @@
 let categoryForm = (value = '') => {
-  let newForm = '<form action="#" method="POST" class="category-form"><input type="text" placeholder="Enter name of category" name="categoryName" required value="' + value + '"><input type="submit"></form>'
+  let newForm = '<form action="#" method="POST" class="category-form"><span class="close-form-btn">&times;</span><input type="text" placeholder="Enter name of category" name="categoryName" required value="' + value + '"><input type="submit"></form>'
 
   return newForm;
 }
