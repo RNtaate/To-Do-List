787f911e83526f70221dbc918d89899d8bc639fd
RNtaate <royhyde14@gmail.com>
2020-12-29 16:05:30 +0300

    Create innerListItemsUpdater method, add default category

--- src/index.js
+++ src/index.js
@@ -5,9 +5,25 @@ let body = document.querySelector('body');
 let catForm;
 
 let categories = [];
+let allToDos = [];
+
 let leftPaneDiv = document.querySelector('.left-pane-div');
 
 leftPaneDiv.innerHTML = '<ul><li class="outer-list-items"><span>All</span></li><li class="outer-list-items category-list"><span>Categories<i class="fa fa-caret-down"></i></span><ul class="inner-item-list"></ul></li></ul><button class="new-category-btn">Create New Category</button>';
+
+let innerListItemsUpdater = () => {
+  let listItems = document.querySelectorAll('.inner-list-items');
+  for(let i = 0; i < listItems.length; i += 1) {
+    listItems[i].addEventListener('click', function(e) {
+      let updatedListItems = document.querySelectorAll('.inner-list-items');
+      for(let j = 0; j < updatedListItems.length; j += 1) {
+        updatedListItems[j].classList.remove('inner-list-items-active');
+      }
+
+      e.target.classList.add('inner-list-items-active');
+    });
+  }
+}
 
 
 let outerListItems = document.querySelectorAll('.outer-list-items > span');
@@ -25,14 +41,21 @@ for(let i = 0; i < outerListItems.length; i += 1) {
       case 'All':
         document.querySelector('.inner-item-list').classList.remove('appear');
         let selectedList = document.querySelector('.selected');
+        let selectedInnerList = document.querySelector('.inner-list-items-active');
+
         if(selectedList != null) {
           selectedList.classList.remove('selected'); 
+        }
+
+        if(selectedInnerList != null) {
+          selectedInnerList.classList.remove('inner-list-items-active');           
         }
         break;
 
       case 'Categories':
         document.querySelector('.inner-item-list').classList.toggle('appear');
         e.target.children[0].classList.toggle('selected');
+        innerListItemsUpdater();
         break;
 
       default:
@@ -58,6 +81,8 @@ let addNewCategoryToList = (name) => {
   document.querySelector('.inner-item-list').appendChild(newListCategory);
 }
 
+addNewCategoryToList('Uncategorised');
+
 let getCategoryFormValues = () => {
   catForm = document.querySelector('.category-form');
 
@@ -72,6 +97,7 @@ let getCategoryFormValues = () => {
     catForm.reset();
     document.querySelector('.category-form-div').style.visibility = 'hidden';
     document.querySelector('.category-form-div').style.opacity = '0';
+    innerListItemsUpdater();
     e.preventDefault();
   });
 }
