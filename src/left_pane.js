77a3d4584a6c4693c79dc17cbcd846bf9f724ae6
rumbie <rumbiemudzie@gmail.com>
2021-01-07 11:02:38 +0200

    Create left_pane.js file, add leftPaneComponents function

---
+++ src/left_pane.js
@@ -0,0 +1,7 @@
+let leftPaneComponent = () => {
+  let elementString = '<ul><li class="outer-list-items"><span>All</span></li><li class="outer-list-items category-list"><span>Categories<i class="fa fa-caret-down"></i></span><ul class="inner-item-list"></ul></li></ul><div class="category-btns"><button class="new-category-btn">Create New Category</button><div class="edit-delete-category-div"><button class="edit-category-btn">Edit Category</button><button class="delete-category-btn">Delete Category</button></div></div>';
+  
+  return elementString;
+}
+
+export default leftPaneComponent;
