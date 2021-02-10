3d2fd6ed9288b286e89e46724e7c70bbddf34359
RNtaate <royhyde14@gmail.com>
2021-02-10 18:37:24 +0300

    Add some negative test cases using .not

--- tests/category.test.js
+++ tests/category.test.js
@@ -2,12 +2,25 @@ import Category from '../src/category';
 
 test('should return category name', () => {
   const myCategory = new Category('Running');
+  const secondCategory = new Category();
+
   expect(myCategory.getName()).toBe('Running');
+  expect(myCategory.getName()).not.toBeUndefined();
+
+  expect(secondCategory.getName()).toBeUndefined();
 });
 
-test.only('should set a category name', () => {
+test('should set a category name', () => {
   const anotherCategory = new Category('Shopping');
+  const secondCategory = new Category();
+
   expect(anotherCategory.getName()).toBe('Shopping');
   expect(anotherCategory.setName('Cleaning')).toBeUndefined();
+  expect(anotherCategory.getName()).not.toBe('Shopping');
   expect(anotherCategory.getName()).toBe('Cleaning');
+
+  expect(secondCategory.getName()).toBeUndefined();
+  expect(secondCategory.setName('Reading')).toBeUndefined();
+  expect(secondCategory.getName()).not.toBeUndefined();
+  expect(secondCategory.getName()).toBe('Reading');
 });
--- tests/task.test.js
+++ tests/task.test.js
@@ -2,16 +2,24 @@ import Task from '../src/task';
 
 const myTask = new Task('Laptop Shopping', 'I will shop with Consultant', '2021-01-12', 'High', 'Shopping');
 
+const anotherTask = new Task();
+
 test('Should return the task title', () => {
   expect(myTask.getTaskTitle()).toBe('Laptop Shopping');
+  expect(myTask.getTaskTitle()).not.toBeUndefined();
+  expect(anotherTask.getTaskTitle()).toBeUndefined();
 });
 
 test('Should return the task Description', () => {
   expect(myTask.getTaskDesc()).toBe('I will shop with Consultant');
+  expect(myTask.getTaskDesc()).toBeDefined();
+  expect(anotherTask.getTaskDesc()).toBeUndefined();
 });
 
 test('Should return the task date', () => {
   expect(myTask.getTaskDate()).toBe('2021-01-12');
+  expect(myTask.getTaskDate()).not.toBeUndefined();
+  expect(anotherTask.getTaskDate()).toBeUndefined();
 });
 
 test('Should return the task Category Name', () => {
@@ -20,6 +28,8 @@ test('Should return the task Category Name', () => {
 
 test('Should return the task Priority', () => {
   expect(myTask.getPriority()).toBe('High');
+  expect(myTask.getPriority()).toBeDefined();
+  expect(anotherTask.getPriority()).toBeUndefined();
 });
 
 test('Should set an entirely new task', () => {
@@ -27,4 +37,10 @@ test('Should set an entirely new task', () => {
   myTask.setTask('Shopping for a house', 'I will meet with agent', '2021-02-02', 'Medium', 'Shopping');
   expect(myTask.getTaskTitle()).toBe('Shopping for a house');
   expect(myTask.getTaskDate()).toBe('2021-02-02');
+
+  myTask.setTask();
+  expect(myTask.getTaskTitle()).not.toBe('Shopping for a house');
+  expect(myTask.getTaskTitle()).toBeUndefined();
+  expect(myTask.getTaskDate()).not.toBe('2021-02-02');
+  expect(myTask.getTaskDate()).not.toBeDefined();
 });
